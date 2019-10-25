cf5051d7d2119357e7f5671372f62f77a5f20e22
danijela <danijela.blace@gmail.com>
2019-10-25 12:51:18 +0200

    skapar objekt i class GuessingGame.java

---
+++ GuessingGame.java
@@ -0,0 +1,11 @@
+import java.io.Console;
+
+public class GuessingGame
+{
+	public static void main(String[] args)
+	{
+	//skapar objekt
+	Guesser guesser = new Guesser(0,1000);
+	guesser.start();//anropar metoden som ligger i class Guesser
+	}
+}
