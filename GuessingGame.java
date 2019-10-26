5fcd4f628993ab90cd334d6a60cbe1d4299be7e7
danijela <danijela.blace@gmail.com>
2019-10-26 15:54:25 +0200

    Task5. The player choose parameters before the game start.Plus: new IllegalArgumentException

--- GuessingGame.java
+++ GuessingGame.java
@@ -1,10 +1,24 @@
 
 import java.io.Console;
+import java.util.Scanner;
 
 public class GuessingGame
 {
 	public static void main(String[] args)
 	{
+	int _low=Integer.parseInt(args[0]);
+	int _high=Integer.parseInt(args[1]);
+	if(_high<_low)
+	throw new IllegalArgumentException("The first parameter must be lower then the second");
+ 	else	
+	{
+	Guesser guesser = new Guesser(_low,_high);
+        guesser.start();//anropar metoden som ligger i class Guesser
+	}
+
+
+
+/* stari kod
 	//declaration and initialization of variables 
 	int _low=0;
 	int _high=1000;
@@ -12,5 +26,6 @@ public class GuessingGame
 	//passing the values of variabels to the constructor
 	Guesser guesser = new Guesser(_low,_high);
 	guesser.start();//anropar metoden som ligger i class Guesser
-	}
+*/	
+}
 }
